8c3e2f5a94d050968fd3b51b8a4f40e9f7615a0d
leshawn-rice <leshawnrice3@gmail.com>
2021-07-27 20:06:12 -0700

    started on a drawer

---
+++ frontend/src/components/Nav/Drawer.js
@@ -0,0 +1,17 @@
+// External Dependencies
+import React from 'react';
+
+// Styles
+import '../../styles/Drawer.scss';
+
+const Drawer = ({ isToggled, links }) => {
+  // needs theme from redux
+
+  return (
+    <div className="Drawer align-center">
+      <p className="header-1">Home</p>
+    </div>
+  )
+}
+
+export default Drawer;
--- frontend/src/components/Routes/Home.js
+++ frontend/src/components/Routes/Home.js
@@ -5,12 +5,7 @@ const Home = () => {
   return (
     <div>
       <div className="row justify-center fadeIn-5">
-        <div className="col">
-          <h1 className="header-1 primary">Welcome.</h1>
-          <p className="text-large dark">Large Text</p>
-          <h1 className="header-2 bg-dark">Header 2</h1>
-          <p className="text bg-primary">Regular Text</p>
-        </div>
+        <p>Hello World</p>
       </div>
     </div>
   );
