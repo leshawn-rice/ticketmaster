c3ffcde2d8c015f56bac63ef51b5484fac20d4d0
leshawn-rice <leshawnrice3@gmail.com>
2021-09-06 13:14:26 -0700

    removed border animation from navbar header and changed favicon

--- frontend/src/components/Nav/Navbar.js
+++ frontend/src/components/Nav/Navbar.js
@@ -12,52 +12,52 @@ import Drawer from './Drawer';
 
 const Navbar = ({ theme }) => {
 
-  return (
-    <Drawer />
-  )
-
-  // let page = 'home';
-  // let bgClass = theme === 'dark' ? 'bg-dark' : 'bg-primary';
-  // let navLogo = theme === 'light' ? logo : darkLogo;
-
-  // const displayDesktop = () => {
-  //   return (
-  //     <div className={`Navbar-Links ${bgClass}`}>
-  //       {NAV_LINKS[page].map(link => (
-  //         <NavLink
-  //           key={`${link.label}-navlink`}
-  //           className={`Navbar-Link ${bgClass}`}
-  //           to={link.href}
-  //         >
-  //           {link.label}
-  //         </NavLink>
-  //       ))}
-  //     </div>
-  //   );
-  // }
-
-  // const displayMobile = () => {
-  // }
-
   // return (
-  //   <div className={`Navbar ${bgClass}`}>
-  //     <div className="Navbar-Home">
-  //       <img className="Navbar-Logo" src={navLogo} alt="nav-logo" />
-  //       <NavLink className={`Navbar-Header ${bgClass}`} to="/">Ticket Master</NavLink>
-  //     </div>
-  //     {/* display === mobile ? displayMobile() : displayDesktop() */}
-  //     <div className={`Navbar-Links ${bgClass}`}>
-  //       {NAV_LINKS[page].map(link => (
-  //         <NavLink
-  //           key={`${link.label}-navlink`}
-  //           className={`Navbar-Link ${bgClass}`}
-  //           to={link.href}
-  //         >
-  //           {link.label}
-  //         </NavLink>
-  //       ))}
-  //     </div>
-  //   </div>
+  //   <Drawer />
   // )
+
+  let page = 'home';
+  let bgClass = theme === 'dark' ? 'bg-dark' : 'bg-primary';
+  let navLogo = theme === 'light' ? logo : darkLogo;
+
+  const displayDesktop = () => {
+    return (
+      <div className={`Navbar-Links ${bgClass}`}>
+        {NAV_LINKS[page].map(link => (
+          <NavLink
+            key={`${link.label}-navlink`}
+            className={`Navbar-Link ${bgClass}`}
+            to={link.href}
+          >
+            {link.label}
+          </NavLink>
+        ))}
+      </div>
+    );
+  }
+
+  const displayMobile = () => {
+  }
+
+  return (
+    <div className={`Navbar ${bgClass}`}>
+      <div className="Navbar-Home">
+        <img className="Navbar-Logo" src={navLogo} alt="nav-logo" />
+        <NavLink className={`Navbar-Header ${bgClass}`} to="/">Ticket Master</NavLink>
+      </div>
+      {/* display === mobile ? displayMobile() : displayDesktop() */}
+      <div className={`Navbar-Links ${bgClass}`}>
+        {NAV_LINKS[page].map(link => (
+          <NavLink
+            key={`${link.label}-navlink`}
+            className={`Navbar-Link ${bgClass}`}
+            to={link.href}
+          >
+            {link.label}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  )
 }
 export default Navbar;
